daef58cc3a60902bfc342364a630910ce851bbda
tbrugnara <tommy.brugnara@gmail.com>
2020-11-03 09:50:53 +0100

    08Multiplo

---
+++ 08Multiplo/08Multiplo.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int x;
+int y;
+
+int main(){
+
+cout<<"Inserici il valore del primo numero: "<<endl;
+cin>>x;
+cout<<"Inserici il valore del secondo numero: "<<endl;
+cin>>y;
+
+
+if (y==0)
+{
+    cout<<"Impossibile"<<endl;
+    return 0;
+}
+else
+{
+    
+
+
+
+if (x%y==0)
+{
+    cout<<x<<" e' multiplo di "<<y<<endl;
+}
+else
+{
+    cout<<x<<"Non e' multiplo di "<<y<<endl;
+}
+return 0;
+}
+}
